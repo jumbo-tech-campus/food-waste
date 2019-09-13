2fb90c4c4c3a20a69a413e05008be85a1e9c26e5
mksengun <mustafa.kucuksengun-cic.netherlands@ibm.com>
2019-09-13 17:39:24 +0200

    list is implemented

--- app/FoodWaste/lib/enter_ingredients_page.dart
+++ app/FoodWaste/lib/enter_ingredients_page.dart
@@ -11,10 +11,30 @@ class EnterIngredientsPage extends StatefulWidget {
 class _EnterIngredientsPageState extends State<EnterIngredientsPage> {
   List<String> _addedIngredients = [];
 
+  final editTextController = TextEditingController();
+
   void _addIngredient(String ingredient) {
     setState(() {
       _addedIngredients.add(ingredient);
     });
+  }
+
+  void _clearEditTextArea() {
+    editTextController.clear();
+  }
+
+  void _showConfirmationAlertDialog(String ingredient) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          // Retrieve the text the that user has entered by using the
+          // TextEditingController.
+          content: Text(
+              ingredient + " added to your list."),
+        );
+      },
+    );
   }
 
   @override
@@ -29,21 +49,53 @@ class _EnterIngredientsPageState extends State<EnterIngredientsPage> {
               children: <Widget>[
                 Flexible(
                   child: TextField(
+                    controller: editTextController,
+                    onSubmitted: (text) {
+                      _addIngredient(text);  // Append Text to the list
+                      _clearEditTextArea();     // Clear the Text area
+                      setState(() {});   // Redraw the Stateful Widget
+                    },
                     decoration:
                         InputDecoration(helperText: "Enter your ingredient"),
                     style: Theme.of(context).textTheme.body1,
                   ),
                 ),
-                Flexible(child: MaterialButton(child: Text("Add"))),
+                Flexible(
+                    child: MaterialButton(
+                  child: Text("Add to list"),
+                  onPressed: () {
+                    _showConfirmationAlertDialog(editTextController.text);
+                    _addIngredient(editTextController.text);
+                    _clearEditTextArea();
+                  },
+                )),
               ],
             ),
+            new Expanded(
+                child: new ListView.builder
+                  (
+                    itemCount: _addedIngredients.length,
+                    itemBuilder: (BuildContext ctxt, int Index) {
+                      return new Text(_addedIngredients[Index]);
+                    }
+                )
+            )
           ],
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => {},
+          onPressed: () {
+            _showConfirmationAlertDialog(editTextController.text);
+          },
           tooltip: 'Increment',
           child: Icon(Icons.add),
           backgroundColor: JumboUI.yellowColor,
         ));
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    editTextController.dispose();
+    super.dispose();
   }
 }
