baa483030f2fe4e5bd15df56cc793898f0d67094
Roelof Reitsma <roelof.reitsma@jumbo.com>
2019-09-14 09:31:02 +0200

    Refactor Recipe object creation into RecipeFactory class
    It eliminates the bug where Recipe objects did not have all properties

--- api/app/controllers/recipes_controller.rb
+++ api/app/controllers/recipes_controller.rb
@@ -1,12 +1,8 @@
 class RecipesController < ApplicationController
   def show
     client = Redis.new(host: 'redis')
-    r = JSON.parse(client.hget('recipes', params[:id]))
+    recipe = RecipeFactory.create_from_json(client.hget('recipes', params[:id]))
 
-    recipe = Recipe.new(id: r['id'], name: r['name'], image_url: r['imageInfo']['primaryView'].first['url'],
-      cooking_time: r['cookingTime'], number_of_portions: r['numberOfPortions'],
-      difficulty_level: r['difficultyLevel'], calories: r['calories']
-    )
     render json: recipe, adapter: :json
   end
 
